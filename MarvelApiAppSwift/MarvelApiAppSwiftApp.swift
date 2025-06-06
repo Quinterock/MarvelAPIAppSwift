c4abdfc1d275ae45dfacbd2e17dfa82457bfd345
Quinterock <luisgqr1975@gmail.com>
2025-06-06 11:58:36 -0600

    First commit

---
+++ MarvelApiAppSwift/MarvelApiAppSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarvelApiAppSwiftApp.swift
+//  MarvelApiAppSwift
+//
+//  Created by Luis Quintero on 06/06/25.
+//
+
+import SwiftUI
+
+@main
+struct MarvelApiAppSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
